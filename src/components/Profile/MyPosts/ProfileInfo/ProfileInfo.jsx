d633c69c87284234dbf44c6055dd96efa6cb3ce9
sergo366 <kozenkovs366@gmail.com>
2020-11-07 14:48:15 +0200

    62 lesson

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
+import Preloader from "../../../common/Preloader/Preloader";
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <Preloader/>
+    }
     return (
         <div>
             <div>
                 <img
                     src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
             </div>
-            <div className={s.descriptionBlock}>
-                ava + description
+            <div className={s.flex}>
+                <div>
+                    <img src={props.profile.photos.large}/>
+                    <div>{props.profile.fullName}</div>
+                </div>
+
+                <div className={s.textBlock}>
+                    <div>{props.profile.lookingForAJob? "Ищу работу":"Уже работаю"}</div>
+                    <div>{props.profile.lookingForAJobDescription}</div>
+                    <div>{props.profile.contacts.twitter}</div>
+                    <div>{props.profile.contacts.facebook}</div>
+                    <div>{props.profile.contacts.instagram}</div>
+                </div>
             </div>
         </div>
     )
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './users.module.css';
 import userPhoto from '../../assets/image/users.png'
+import {NavLink} from "react-router-dom";
+import * as axios from "axios";
 
 let Users = (props) => {
 
@@ -26,15 +28,40 @@ let Users = (props) => {
         {props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.usersPhoto}/>
+                        <NavLink to={'/profile/' + u.id}>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.usersPhoto}/>
+                        </NavLink>
                     </div>
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+                                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "565e165f-881f-4b0d-a639-bb2e44769ce8"
+                                    }
+                                })
+                                    .then(response => {
+                                        if (response.data.resultCode === 0) {
+                                            props.unfollow(u.id)
+                                        }
+                                    });
+
                             }}>Unfollow</button>
+
                             : <button onClick={() => {
-                                props.follow(u.id)
+
+                                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {} , {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "565e165f-881f-4b0d-a639-bb2e44769ce8"
+                                    }
+                                })
+                                    .then(response => {
+                                       if (response.data.resultCode === 0) {
+                                           props.follow(u.id)
+                                       }
+                                    });
                             }}>Follow</button>}
                     </div>
                 </span>
