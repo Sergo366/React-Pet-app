f6eaed23bf4ba792d83784a4a431ac0e0be734ed
sergo366 <kozenkovs366@gmail.com>
2020-12-28 14:54:45 +0200

    96 lesson (add photo in profile)

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../../common/Preloader/Preloader";
-import ProfileStatus from "./ProfileStatus";
 import ProfileStatusWithHook from "./ProfileStatusWithHook";
+import userPhoto from '../../../../assets/image/I.jpg'
 
-const ProfileInfo = ({profile, status, updateStatus}) => {
+
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
 
     if (!profile) {
         return <Preloader/>
     }
+
+    const onMainPhotoSelected = (e) =>{
+       if (e.target.files.length){
+           savePhoto(e.target.files[0])
+       }
+    }
+
     return (
             <div>
-                <div>
-                    <img src={profile.photos.large}/>
+                <div >
+                    <img src={profile.photos.large || userPhoto} className={s.mainPhoto}/>
                     <div>{profile.fullName}</div>
+                    {isOwner && <input type={"file"} onChange={onMainPhotoSelected}/>}
                 </div>
                 <div className={s.textBlock}>
                     <div>{profile.lookingForAJob? "Ищу работу":"Уже работаю"}</div>
